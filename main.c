a6a82ada1b95491a4006b01e12b991a878b5777a
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2024-07-09 16:01:04 +0100

    check quote errors

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mskhairi <mskhairi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 18:33:23 by mskhairi          #+#    #+#             */
-/*   Updated: 2024/07/09 15:25:47 by mskhairi         ###   ########.fr       */
+/*   Updated: 2024/07/09 15:35:03 by mskhairi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ int	main(void)
         tmp = readline("[minishell]::~>");
 		input = ft_strtrim(tmp, "\t \f\v\n\r");
 		free(tmp);
-		//ft_strtrim() for delete the space 
         if (!input)
             printf("Exit\n");
         if (!ft_strncmp(input, "",ft_strlen(input)) || is_empty(input))
