6d561cd7bc675a4326f0ab1631a9aa20506ff661
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2024-07-20 19:28:21 +0100

    modification in ft_new_list.c  function

--- parser/organize.c
+++ parser/organize.c
@@ -6,7 +6,7 @@
 /*   By: mskhairi <mskhairi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 14:37:20 by rmarzouk          #+#    #+#             */
-/*   Updated: 2024/07/20 17:58:27 by mskhairi         ###   ########.fr       */
+/*   Updated: 2024/07/20 19:20:21 by mskhairi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,34 @@
 
 // create another linked list with expander values and removed of quotations
 
+int	join_limiter(int type, int state)
+{
+	if ((type == WHITE_SPACE && state == GENERAL) || (type != WORD
+			&& type != QOUTE && type != DOUBLE_QUOTE))
+		return (0);
+	return (1);
+}
+
 void	ft_new_list(t_item *list, t_item **new_list)
 {
-	char *join_content;
-	int type;
+	char	*join_content;
+	int		type;
 
 	join_content = NULL;
 	type = WORD;
 	while (list)
 	{
-		if (list && (!(list->type == WHITE_SPACE && list->state == GENERAL)) && list->type != QOUTE && list->type != DOUBLE_QUOTE)
+		if (list && (!(list->type == WHITE_SPACE && list->state == GENERAL))
+			&& list->type != QOUTE && list->type != DOUBLE_QUOTE)
 			join_content = ft_strjoin(join_content, list->content);
-		if (!list->next || (list->next && join_content && (list->next->type == WHITE_SPACE && list->next->state == GENERAL)))
+		if (!list->next || (list->next && join_content
+				&& (!join_limiter(list->next->type, list->next->state)
+					|| !join_limiter(list->type, 0))))
 		{
-			if (list->type == REDIR_IN || list->type == REDIR_OUT || list->type == DREDIR_OUT || list->type == HERE_DOC || list->type == PIPE_LINE)
+			if (!join_limiter(list->type, 0))
 				type = list->type;
-			add_back_items(new_list, new_item(join_content, ft_strlen(join_content), type, GENERAL));
+			add_back_items(new_list, new_item(join_content,
+						ft_strlen(join_content), type, GENERAL));
 			free(join_content);
 			join_content = NULL;
 			type = WORD;
@@ -38,21 +50,21 @@ void	ft_new_list(t_item *list, t_item **new_list)
 	}
 }
 
-void	last_tokinization(t_item	*list)
+void	last_tokinization(t_item *list)
 {
-	t_item *tmp;
+	t_item	*tmp;
 
 	tmp = list;
 	while (tmp)
-	{	
+	{
 		if (tmp->type == REDIR_IN)
-				tmp->next->type = REDIR_IN_FILE;
+			tmp->next->type = REDIR_IN_FILE;
 		else if (tmp->type == REDIR_OUT)
-				tmp->next->type = REDIR_OUT_FILE;
+			tmp->next->type = REDIR_OUT_FILE;
 		else if (tmp->type == DREDIR_OUT)
-				tmp->next->type = DREDIR_OUT_FILE;
+			tmp->next->type = DREDIR_OUT_FILE;
 		else if (tmp->type == HERE_DOC)
-				tmp->next->type = HERE_DOC_LIMITER;
+			tmp->next->type = HERE_DOC_LIMITER;
 		tmp = tmp->next;
 	}
 }
